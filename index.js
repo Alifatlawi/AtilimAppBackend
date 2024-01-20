f1de4f694747016b2cc83a77aca48593f48b5d74
Ali Al-Fatlawi <alawi_amer@yahoo.co.uk>
2024-01-21 02:17:11 +0300

    first commit

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(express.json());
 
 
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+
 app.get('/courses', async (req, res) => {
     const url = 'https://atilim.edupage.org/timetable/server/regulartt.js?__func=regularttGetData'; // Replace with your URL
     try {
@@ -23,7 +28,7 @@ app.get('/courses', async (req, res) => {
     }
 });
 
-const port = 3000;
+const port = 4000;
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
 });
