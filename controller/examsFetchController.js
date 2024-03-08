a28dd66e692fa98bc4ecebd7c2c61ad17270403e
Ali Al-Fatlawi <alawi_amer@yahoo.co.uk>
2024-03-08 12:09:26 +0300

    fix CoursesCode for swiftUI

--- controller/examsFetchController.js
+++ controller/examsFetchController.js
@@ -6,31 +6,40 @@ const { saveDataToFile } = require('../utils/fileHelpers');
 async function fetchMidExams() {
     try {
         const url = 'https://dersprogramiyukle.atilim.edu.tr/20232024arasinavbahar/muh/index_files/sheet001.htm';
-        // const url = 'https://dersprogramiyukle.atilim.edu.tr/20232024arasinavbahar/servis/index_files/sheet001.htm'
         let response = await axios.get(url);
         const html = response.data;
         const $ = cheerio.load(html);
-        const courses = [];
+        const examsByCourse = {};
 
         $('tr').each((i, el) => {
-            if (i > 0) { // Assuming you want to skip the first row/header
+            if (i > 0) { // Skipping the header
                 const courseCode = $(el).find('td').eq(2).text().trim();
-                const date = $(el).find('td').eq(4).text().trim(); 
+                const date = $(el).find('td').eq(4).text().trim();
                 const time = $(el).find('td').eq(6).text().trim();
 
-                // Only add courses with a course code
-                if(courseCode && courseCode != "DERS KODU/ COURSE CODE" && courseCode != 'DERS\n  KODU/              COURSE CODE') {
-                    courses.push({ courseCode, date, time });
+                if (courseCode && courseCode != "DERS KODU/ COURSE CODE" && courseCode != 'DERS\n  KODU/              COURSE CODE' && courseCode != "DERS\n  KODU/              COURSE CODE") {
+                    if (!examsByCourse[courseCode]) {
+                        examsByCourse[courseCode] = [];
+                    }
+                    examsByCourse[courseCode].push({ date, time });
                 }
             }
         });
 
+        // Generating courses array with modified course codes for duplicate exams
+        const courses = [];
+        Object.keys(examsByCourse).forEach((courseCode, index) => {
+            examsByCourse[courseCode].forEach((exam, examIndex) => {
+                const modifiedCourseCode = examsByCourse[courseCode].length > 1 ? `${courseCode}-${examIndex + 1}` : courseCode;
+                courses.push({ courseCode: modifiedCourseCode, ...exam });
+            });
+        });
 
         const filePath = './public/EngMidExams.json'; // Adjust the path as needed
         saveDataToFile(courses, filePath);
     } catch (error) {
         console.error(error);
-        throw error; // Throw the error to be caught by the caller
+        throw error; // Rethrowing the error for the caller to handle
     }
 }
 
